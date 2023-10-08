ee94d65305eb7eedb638cc1a94e80621bda0b8b1
Bubba Gray <jtgray0828@yahoo.com>
2023-10-07 21:46:11 -0400

    week 14

---
+++ target/classes/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO customer (customer_id, customer_email, customer_first_name, customer_last_name) VALUES (5, 'davejackson@gmail.com', 'Dave', 'Jackson');
+INSERT INTO customer (customer_id, customer_email, customer_first_name, customer_last_name) VALUES (1, 'johndoe@gmail.com', 'John', 'Doe');
+INSERT INTO pet_store (pet_store_id, pet_store_phone, pet_store_zip, pet_store_address, pet_store_city, pet_store_name, pet_store_state) VALUES (169, 7778888, 57569, '1 Pets Drive', 'Bloomington', 'Pets-A-Plenty', 'Indiana');
+INSERT INTO employee (employee_id, employee_phone, pet_store_id, employee_first_name, employee_job_title, employee_last_name) VALUES (157, 7779999, 169, 'Jack', 'Manager', 'Travis');
+INSERT INTO customer (customer_id, customer_email, customer_first_name, customer_last_name) VALUES (2, 'jacobvincent@gmail.com', 'Jacob', 'Vincent');
+INSERT INTO pet_store (pet_store_id, pet_store_phone, pet_store_zip, pet_store_address, pet_store_city, pet_store_name, pet_store_state) VALUES (170, 777222, 57569, '15 Jacobson Ave', 'Bloomington', 'Pets-and-Things', 'Indiana');
+INSERT INTO employee (employee_id, employee_phone, pet_store_id, employee_first_name, employee_job_title, employee_last_name) VALUES (158, 7772222, 170, 'Jason', 'Manager', 'Gray');
+INSERT INTO customer (customer_id, customer_email, customer_first_name, customer_last_name) VALUES (3, 'maryjohnson@gmail.com', 'Mary', 'Johnson');
+INSERT INTO customer (customer_id, customer_email, customer_first_name, customer_last_name) VALUES (4, 'maxjohnson@gmail.com', 'Max', 'Johnson');
